d0e581f3ceb48a4b18e7739c81c87278d244e7f8
Gage Harmon <gaharmon1@gmail.com>
2023-02-16 11:39:00 -0700

    FIXED EVERYTHING, Submit buttons now work and update the displayed along with the navbar, tons of CSS change.

--- script.js
+++ script.js
@@ -8,64 +8,47 @@ let form = document.querySelector('#pizza-review-form');
 let cardList = document.querySelector('#comment-list');
 
 // Fetch pizza data and create slide show
+
 fetch('http://localhost:3000/pizza')
     .then(response => response.json())
     .then(pizzaData => {
         createSlideShow(pizzaData);
-        // handleLikes();
-        handleComment();
-        openReviewBox();
+        openSubmitBox();
+        addNewPizza();
+        addNewPizzaImage();
+        addNewPizzaFeature();
     });
 
 // Define function to create slide show
 function createSlideShow(pizzaData) {
-    selectedPizza = pizzaData;
-    let pizzaName = document.getElementById('pizza-name');
-    // let prevButton = document.getElementById('prev-pizza');
-    // let nextButton = document.getElementById('next-pizza');
-
-    let currentPizzaIndex = 0;
-
-
-    // prevButton.addEventListener('click', () => {
-    //     currentPizzaIndex = (currentPizzaIndex - 1 + pizzaData.length) % pizzaData.length;
-    //     updateDisplayedPizza();
-    // });
-
-    // nextButton.addEventListener('click', () => {
-    //     currentPizzaIndex = (currentPizzaIndex + 1) % pizzaData.length;
-    //     updateDisplayedPizza();
-    // });
-
-    pizzaData.forEach(pizza => {
-        let image = document.createElement('img');
-        image.src = pizza.image;
-
-        pizzaList.appendChild(image);
-
-        image.addEventListener('click', () => {
-            currentPizzaIndex = pizzaData.indexOf(pizza);
-
-            updateDisplayedPizza();
+    pizzaData.forEach(item => {
+        let pizzaImage = document.createElement('img');
+        pizzaImage.src = item.image;
+        pizzaList.appendChild(pizzaImage);
+        pizzaImage.addEventListener('click', () => {
+            updateDisplayedPizza(item);
         });
+        // pizzaImage.addEventListener('mouseover', () => {
+        //     pizzaImage.style.transform = 'scale(1.5)';
+        // });
 
-        image.addEventListener('mouseover', () => {
-            image.style.transform = 'scale(1.5)';
-        });
-
-        image.addEventListener('mouseout', () => {
-            image.style.transform = 'scale(1.0)';
-        });
+        // pizzaImage.addEventListener('mouseout', () => {
+        //     pizzaImage.style.transform = 'scale(1.0)';
+        // });
     });
+    updateDisplayedPizza(pizzaData[0]);
+};
 
-    function updateDisplayedPizza() {
-        let pizza = pizzaData[currentPizzaIndex];
-        pizzaImage.src = pizza.image;
-        pizzaName.textContent = pizza.name;
-        pizzaFeature.textContent = pizza.feature;
-    }
+function updateDisplayedPizza(pizzaData) {
+    selectedPizza = pizzaData;
 
-    updateDisplayedPizza();
+    let pizzaDisplayImage = document.querySelector("#pizza-image")
+    let pizzaDisplayName = document.querySelector("#pizza-name");
+    let pizzaDisplayFeature = document.querySelector("#pizza-feature")
+
+    pizzaDisplayImage.src = selectedPizza.image;
+    pizzaDisplayName.textContent = selectedPizza.name;
+    pizzaDisplayFeature.textContent = `Known for ${selectedPizza.feature}`;
 }
 
 
@@ -81,55 +64,42 @@ toggleButton.addEventListener('click', function () {
 
 
 
-///// LIKE BUTTON /////
-//  #1 (with PUT) //
-// function handleLikes() {
-//     const likeButton = document.getElementById('like-button');
-//     likeButton.addEventListener('click', () => {
-//         selectedPizza.likes += 1; // Increment the likes property of the selected pizza by one
-//         fetch('http://localhost:3000/pizza', {
-//             method: 'PUT',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify(selectedPizza) // Update the db.json object with the new pizza data
-//         });
-//     });
-// }
+/// UPDATE PIZZA BOX //////
+function addNewPizza() {
+    let newPizzaForm = document.querySelector('#new-pizza-form');
+    newPizzaForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        selectedPizza.name = event.target['pizza-name-input'].value;
+        updateDisplayedPizza(selectedPizza);
+    });
+}
 
-//  #2 (OG) //
-// function handleLikes() {
-//     selectedPizza = pizzaData;
-//     let likeCount = document.querySelector("#like-count");
-//     let likeButton = document.querySelector('#like-button');
-//     likeButton.addEventListener('click', () => {
-//         if (selectedPizza.likes < 2000) {
-//             selectedPizza.likes++;
-//             likeCount.textContent = selectedPizza.likes;
-//         }
-//     })
-// }
-///// END LIKE BUTTON /////
+function addNewPizzaImage() {
+    let newPizzaForm = document.querySelector("#new-pizza-form");
+    newPizzaForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let pizzaImage = document.createElement("img");
+        pizzaImage.src = event.target['pizza-image-input'].value;
+        pizzaList.appendChild(pizzaImage);
+        selectedPizza.image = event.target['pizza-image-input'].value;
+        updateDisplayedPizza(selectedPizza);
+    })
+}
 
-
-/// COMMENT BOX //////
-function handleComment() {
-    let form = document.querySelector('form');
-    let commentList = document.querySelector('#comment-list');
-    form.addEventListener('submit', (event) => {
-        event.preventDefault(); // prevent the form from submitting normally
-        let li = document.createElement('li');
-        li.textContent = event.target['pizza-review-input'].value;
-        commentList.appendChild(li);
-        form.reset();
+function addNewPizzaFeature() {
+    let newPizzaForm = document.querySelector('#new-pizza-form');
+    newPizzaForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        selectedPizza.feature = event.target['pizza-feature-input'].value;
+        updateDisplayedPizza(selectedPizza);
     });
 }
 
 
 ///// OPEN REVIEW BUTTON /////
 
-function openReviewBox() {
-    let reviewButton = document.getElementById('open-review-button');
+function openSubmitBox() {
+    let reviewButton = document.getElementById('open-submit-button');
     let reviewBox = document.getElementById('review-box');
     let isOpen = false; // track whether the review box is currently open or not
 
